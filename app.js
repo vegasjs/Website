36f02d79409bf8d432fcb85faf8d4aafa86584ed
Pawel Szymczkowski <pawel@jeff-newburns-macbook-pro.local>
2011-03-09 01:25:31 -0800

    Added basic member list functionality via Meetup

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var app = require('express').createServer();
+var app    = require('express').createServer(),
+	config = require('./config'),
+	Meetup = require('./lib/meetup');
 
 // Configuration
+
+var meetup = new Meetup({
+	key: config.meetup.key
+});
 
 // View engine wrapper for Handlebars ( https://github.com/donpark/hbs )
 
@@ -11,12 +17,27 @@ app.set('view engine', 'hbs');
 app.get('/', function(req, res){
     var options = {
         locals: {
-            title: "VegasJS - Las Vegas Area JavaScript User Group",
-            members: ["Jimmy", "Pawel", "Alex", "Dylan", "Daniel", "RayMorgan"],
+            title: "VegasJS - Las Vegas Area JavaScript User Group"
         }
     };
     res.render('index', options);
 });
+
+app.get('/members', function(req, res){
+    var options = {
+		locals: {
+			title: 'VegasJS Member List (via the Meetup.com API)'
+		}
+    };
+	meetup.memberList( config.meetup.group, function(members) {
+		options.locals.members = members;
+		res.render('members', options);
+	});
+});
+
+// app.get('/member/:id'), function(req,res) {
+// 	console.log( req.params.id );
+// });
 
 
 app.listen(8080);
