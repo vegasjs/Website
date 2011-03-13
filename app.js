6f3d78235e8e87f0133d4529e84705c589a2e8c0
Daniel lopez <daniel.a.lopez@gmail.com>
2011-03-12 19:35:22 -0800

    added method to meetup.js to get events and attendees also added utility object with a underscore.js dependency

--- app.js
+++ app.js
@@ -1,38 +1,49 @@
-var app    = require('express').createServer(),
-	config = require('./config'),
-	Meetup = require('./lib/meetup');
+/* vi: set tabstop=2 shiftwidth=2 expandtab */
+var app = require('express').createServer(),
+    config = require('./config'),
+    Meetup = require('./lib/meetup');
 
 // Configuration
-
 var meetup = new Meetup({
-	key: config.meetup.key
+  group: 'vegasjs',
+  key: config.meetup.key
 });
 
 // View engine wrapper for Handlebars ( https://github.com/donpark/hbs )
-
 app.set('view engine', 'hbs');
 
 // Routes 
-
 app.get('/', function(req, res){
-    var options = {
-        locals: {
-            title: "VegasJS - Las Vegas Area JavaScript User Group"
-        }
-    };
-    res.render('index', options);
+  var options = {
+    locals: {
+      title: "VegasJS - Las Vegas Area JavaScript User Group"
+    }
+  };
+  res.render('index', options);
 });
 
 app.get('/members', function(req, res){
-    var options = {
-		locals: {
-			title: 'VegasJS Member List (via the Meetup.com API)'
-		}
-    };
-	meetup.memberList( config.meetup.group, function(members) {
-		options.locals.members = members;
-		res.render('members', options);
-	});
+  var options = {
+    locals: {
+      title: 'VegasJS Member List (via the Meetup.com API)'
+    }
+  };
+  meetup.memberList(function(members) {
+    options.locals.members = members;
+    res.render('members', options);
+  });
+});
+
+app.get('/events', function(req, res){
+  var options = {
+    locals: {
+      title: 'VegasJS Events (via the Meetup.com API)'
+    }
+  };
+  meetup.events(function(events) {
+    options.locals.events = events;
+    res.render('events', options);
+  });
 });
 
 // app.get('/member/:id'), function(req,res) {
@@ -41,3 +52,4 @@ app.get('/members', function(req, res){
 
 
 app.listen(8080);
+
