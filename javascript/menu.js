07316596c3d1c6a300c3cc4c77d131f34bb1d888
Pawel Szymczykowski <makenai@gmail.com>
2015-01-10 13:01:34 -0800

    Cursor

--- javascript/menu.js
+++ javascript/menu.js
@@ -2,13 +2,15 @@ $(function() {
 
 	$('#menu a').click(function(e) {
 		var id = $(this).attr('href');
+		$('#menu a').removeClass('current');
+		$(this).addClass('current');
 		$('.dialog').hide();
 		$(id).show();
 		e.preventDefault();
 	});
 
 	// Prevent double click selection except for in dialog
-	$(window).on('mousedown', function(e) {	
+	$(window).on('mousedown', function(e) {
 		if ( !$(e.target).is('.dialog') ) {
 			e.preventDefault();
 			return false;
